d0aae27da789a8d282f2da6590fbd66ab152bb6c
meiyulu <meiyulu@yqjr.com.cn>
2019-09-04 09:43:37 +0800

    提交全选反选

--- pages/lengthlearn/learndetail.js
+++ pages/lengthlearn/learndetail.js
@@ -14,7 +14,7 @@ Page({
           { name: 'B', value: '《论语·雍也》'},
           { name: 'C', value: '《说菀·指武》'},
           { name: 'D', value: '《周礼》'},
-          { name: 'E', value: '《周礼》123' }
+          { name: 'E', value: '《周礼》123'}
         ]
       },
       {
@@ -22,10 +22,10 @@ Page({
         id: 2,
         type: 1,
         question: [
-          { name: 'A', value: '《礼记·中庸》' },
-          { name: 'B', value: '《论语·雍也》' },
-          { name: 'C', value: '《说菀·指武》' },
-          { name: 'D', value: '《周礼》' }
+          { name: 'A', value: '《礼记·中庸》'},
+          { name: 'B', value: '《论语·雍也》'},
+          { name: 'C', value: '《说菀·指武》'},
+          { name: 'D', value: '《周礼》'}
         ]
       },
       {
@@ -41,17 +41,45 @@ Page({
       }
     ]
   },
-    
+  // 给每一题添加选中的属性
+  changeques:function(){
+    const quesitemschecked = this.data.quesitems;
+    for (let i in quesitemschecked) {
+      quesitemschecked[i].danchecked = '';
+    }
+    // console.log(quesitemschecked)
+  },
+  //单选确定全选返选
   inputchecked(e){
     const { ques,check} = e.currentTarget.dataset;
     console.log(ques,check)
-   
+    const changelist = this.data.quesitems;
+    for (let i in changelist){
+      if (ques==i){
+        changelist[i].danchecked = check
+      }
+      // changelist[i].danchecked = check
+      // for(let a in changelist[i].question){
+      //   console.log(a);
+      //   if (ques==a){
+      //     changelist[i].danchecked = check
+      //   }
+      // }
+    }
+    this.setData({
+      quesitems: changelist
+    })
+    console.log(changelist)
   },  
   // 关闭弹框
   closehint:function(){
     this.setData({
       showhint:false
     })
+  },
+  onLoad:function(options){
+    this.changeques();
   }
+
 })
  
